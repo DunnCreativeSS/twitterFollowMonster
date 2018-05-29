e0559cdcfeda1a0b24c1a276f63163b77e0e7505
Jarett Dunn <jarettrsdunn+github@gmail.com>
2018-05-29 20:49:56 -0300

    log

--- controllers/api.js
+++ controllers/api.js
@@ -397,8 +397,9 @@ exports.followTwitter = (req, res, next) => {
 			T.get('trends/place', {
 				id: '1'
 			}, (err, reply) => {
+				console.log(reply.trends);
 				for (var status in reply.trends) {
-					console.log(reply.trends[status]);
+					console.log(reply.trends[status].query);
 					query.push(reply.trends[status].query);
 				}
 				for (var q in query){
